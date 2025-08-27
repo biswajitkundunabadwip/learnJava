5886768d60d020fc438c65c0861d6fe49b01e365
Biswajit Kundu <biswajit.kundu9@gmail.com>
2025-08-27 18:24:28 +0530

    added validation for lambda expression

---
+++ src/demo/learn/innerclass/LambdaExpressionDemo.java
@@ -0,0 +1,15 @@
+package demo.learn.innerclass;
+@FunctionalInterface
+interface FlyingObject {
+	void fly();
+}
+
+public class LambdaExpressionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FlyingObject flyingObject= ()->System.out.println("Aeroplane flying");
+		flyingObject.fly();
+	}
+
+}
