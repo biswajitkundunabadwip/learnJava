cbd89e0ddbdba0a84940784f4fd4b354b4c7ab2b
Biswajit Kundu <biswajit.kundu9@gmail.com>
2025-08-30 10:54:27 +0530

    added thread synchronized concept

--- src/demo/learn/threads/RunnableVsThread.java
+++ src/demo/learn/threads/RunnableVsThread.java
@@ -1,38 +1,48 @@
 package demo.learn.threads;
-class AB implements Runnable{
-	public void run() {
-		for(int i =0;i<10;i++) {
-			dispA();
-		}
-	}
-	
-	public void dispA() {
-		System.out.println("hi");
+
+class Counter {
+	int count;
+
+	public synchronized void  increment() {
+		count++;
 	}
 }
 
-class BA implements Runnable{
-	public void run() {
-		for(int i =0;i<10;i++) {
-			dispB();
-		}
-
-	}
-	public void dispB() {
-		System.out.println("Hello");
-	}
-}
 public class RunnableVsThread {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		Runnable obj1= new AB();
-		Runnable obj2= new BA();
-		
-		Thread t1= new Thread(obj1);
-		Thread t2= new Thread(obj2);
+		Counter c = new Counter();
+
+		Runnable obj1 = new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				for (int i = 0; i < 1000; i++) {
+					c.increment();
+				}
+			}
+		};
+		Runnable obj2 = new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				for (int i = 0; i < 1000; i++) {
+					c.increment();
+				}
+			}
+		};
+
+		Thread t1 = new Thread(obj1);
+		Thread t2 = new Thread(obj2);
 		t1.start();
 		t2.start();
+		t1.join();
+		t2.join();
+
+		System.out.println(c.count);
 	}
 
 }
