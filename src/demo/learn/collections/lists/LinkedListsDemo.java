7b9eb2414cc7e22354ab63f5bb2275f9c6686eb9
Biswajit Kundu <biswajit.kundu9@gmail.com>
2025-09-01 21:41:42 +0530

    added collection data

---
+++ src/demo/learn/collections/lists/LinkedListsDemo.java
@@ -0,0 +1,15 @@
+package demo.learn.collections.lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedListsDemo {
+
+	public static void main(String[] args) {
+		List<String> fruits = List.of("Apple","Banana","Grapes");
+		
+		List<String> fruitsNewLinkedList = new ArrayList<>(fruits);
+		System.out.println("To print all the list "+fruitsNewLinkedList);
+	}
+
+}
