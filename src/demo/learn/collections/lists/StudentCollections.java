22b1e1a99907c17e9b1cf761e0b978570aef4213
Biswajit Kundu <biswajit.kundu9@gmail.com>
2025-09-03 09:56:44 +0530

    added collection,queue

---
+++ src/demo/learn/collections/lists/StudentCollections.java
@@ -0,0 +1,39 @@
+package demo.learn.collections.lists;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+class DescendingStudents implements Comparator<Student>{
+
+	@Override
+	public int compare(Student student1, Student student2) {
+		// TODO Auto-generated method stub
+		
+		return Integer.compare(student2.getId(),student1.getId());
+	}
+	
+}
+
+public class StudentCollections {
+	public static void main(String[] args) {
+		Student s1= new Student(1, "BK");
+		Student s2= new Student(2, "RK");
+		Student s3= new Student(3, "SM");
+		List<Student> studentCollections = new ArrayList<Student>();
+		studentCollections.add(s1);
+		studentCollections.add(s2);
+		studentCollections.add(s3);
+
+		System.out.println("Student collections : "+studentCollections);
+		
+		Collections.sort(studentCollections);
+		System.out.println("Ascending order on id : "+studentCollections);
+		System.out.println("-----------------------------------------");
+//		Collections.sort(studentCollections,new DescendingStudents());
+		studentCollections.sort(new DescendingStudents());
+		System.out.println("Dscending order on id : "+studentCollections);
+		
+		
+	}
+}
---
+++ src/demo/learn/collections/set/TreesetDemo.java
@@ -0,0 +1,34 @@
+package demo.learn.collections.set;
+
+import java.util.TreeSet;
+
+public class TreesetDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet<Integer> treesetDemo = new TreeSet<Integer>();
+		treesetDemo.add(29);
+		treesetDemo.add(82);
+		treesetDemo.add(45);
+		treesetDemo.add(52);
+		treesetDemo.add(21);
+		treesetDemo.add(2);
+		treesetDemo.add(145);
+		System.out.println(treesetDemo);
+		System.out.println(treesetDemo.floor(45));
+		System.out.println(treesetDemo.lower(45));
+		System.out.println(treesetDemo.ceiling(45));
+		System.out.println(treesetDemo.higher(45));
+		
+		/*
+		| Method Call   | Meaning               | Result                         |
+		| ------------- | --------------------- | ------------------------------ |
+		| `treesetDemo` | Print full TreeSet    | `[2, 21, 29, 45, 52, 82, 145]` |
+		| `floor(45)`   | Greatest element ≤ 45 | `45`                           |
+		| `lower(45)`   | Greatest element < 45 | `29`                           |
+		| `ceiling(45)` | Smallest element ≥ 45 | `45`                           |
+		| `higher(45)`  | Smallest element > 45 | `52`                           |
+*/
+	}
+
+}
